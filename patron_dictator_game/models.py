3e6fdcf8c72088b60d577e75dde33e26ebbaf36c
pachikato <vge008kh@student.econ.osaka-u.ac.jp>
2020-07-11 12:04:39 +0900

    make SD treatment - Develop PDG by oTree #1

--- patron_dictator_game/models.py
+++ patron_dictator_game/models.py
@@ -19,8 +19,11 @@ Your app description
 
 class Constants(BaseConstants):
     name_in_url = 'patron_dictator_game'
-    players_per_group = None
+    players_per_group = 3
     num_rounds = 1
+
+    endowment = c(100)
+    maximum_allocate = c(2)
 
 
 class Subsession(BaseSubsession):
@@ -28,8 +31,34 @@ class Subsession(BaseSubsession):
 
 
 class Group(BaseGroup):
-    pass
+    send = models.CurrencyField(
+        label = "Send money to Dictator",
+        min = 0, max = Constants.endowment
+    )
+
+    allocation = models.FloatField(
+        label = "Allocation rule determined by Dictator",
+        min = 0, max = Constants.maximum_allocate
+    )
+
+    def set_payoffs(self):
+        patron = self.get_player_by_role('patron')
+        dictator = self.get_player_by_role('dictator')
+        receiver = self.get_player_by_role('receiver')
+        patron.payoff = Constants.endowment - self.send
+        dictator.payoff = Constants.endowment - (1 - self.allocation) * self.send
+        receiver.payoff = self.send * self.allocation
 
 
 class Player(BasePlayer):
-    pass
+
+    send = models.CurrencyField()
+    allocation = models.FloatField()
+    
+    def role(self):
+        if self.id_in_group == 1:
+            return "patron"
+        if self.id_in_group == 2:
+            return "dictator"
+        if self.id_in_group == 3:
+            return "receiver"
--- patron_dictator_game/pages.py
+++ patron_dictator_game/pages.py
@@ -3,16 +3,47 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 
-class MyPage(Page):
+class Investment(Page):
+    
+    form_model = 'group'
+    form_fields = ['send']
+
+    def is_displayed(self):
+        return self.player.id_in_group == 1
+
+class Allocation(Page):
+    
+    form_model = 'group'
+    form_fields = ['allocation']
+
+    def is_displayed(self):
+        return self.player.id_in_group == 2
+    
+    def vars_for_template(self):
+        return dict(
+            send_amount = self.group.send
+        )
+
+class WaitForPatron(WaitPage):
     pass
 
+class WaitForDictator(WaitPage):
+    pass
 
 class ResultsWaitPage(WaitPage):
-    pass
+    
+    after_all_players_arrive = 'set_payoffs'
 
 
 class Results(Page):
     pass
 
 
-page_sequence = [MyPage, ResultsWaitPage, Results]
+page_sequence = [
+    WaitForDictator,
+    Investment,
+    WaitForPatron,
+    Allocation,
+    ResultsWaitPage, 
+    Results
+]
