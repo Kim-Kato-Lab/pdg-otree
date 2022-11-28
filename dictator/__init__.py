02ecfc5441dc21796be590dbc116c6b04053fa90
KatoPachi <63113684+KatoPachi@users.noreply.github.com>
2022-11-28 21:22:13 +0900

    fix dictator game

--- dictator/__init__.py
+++ dictator/__init__.py
@@ -13,22 +13,20 @@ S285-S300.
 class C(BaseConstants):
     NAME_IN_URL = 'dictator'
     PLAYERS_PER_GROUP = 2
-    NUM_ROUNDS = 1
-    INSTRUCTIONS_TEMPLATE = 'dictator/instructions.html'
-    # Initial amount allocated to the dictator
+    NUM_ROUNDS = 2
     ENDOWMENT = cu(100)
+    PATRON_ROLE = "Patron"
+    RECEIVER_ROLE = "Receiver"
 
 
 class Subsession(BaseSubsession):
     pass
 
+def creating_session(subsession: Subsession):
+    subsession.group_randomly()
 
 class Group(BaseGroup):
-    kept = models.CurrencyField(
-        doc="""Amount dictator decided to keep for himself""",
-        min=0,
-        max=C.ENDOWMENT,
-    )
+    send = models.IntegerField(min=0, max=C.ENDOWMENT)
 
 
 class Player(BasePlayer):
@@ -37,34 +35,66 @@ class Player(BasePlayer):
 
 # FUNCTIONS
 def set_payoffs(group: Group):
-    p1 = group.get_player_by_id(1)
-    p2 = group.get_player_by_id(2)
-    p1.payoff = group.kept
-    p2.payoff = C.ENDOWMENT - group.kept
+    patron = group.get_player_by_role(C.PATRON_ROLE)
+    receiver = group.get_player_by_role(C.RECEIVER_ROLE)
+    patron.payoff = C.ENDOWMENT - group.send
+    receiver.payoff = group.send
 
 
 # PAGES
-class Introduction(Page):
+class Role(Page):
     pass
 
-
-class Offer(Page):
+class WaitRoleCheck(WaitPage):
+    body_text = """
+    あなたのグループのメンバーが役割を確認しています。
+    しばらくお待ちください。
+    """
+class Send(Page):
     form_model = 'group'
-    form_fields = ['kept']
+    form_fields = ['send']
 
     @staticmethod
     def is_displayed(player: Player):
-        return player.id_in_group == 1
-
+        return player.role == C.PATRON_ROLE
 
 class ResultsWaitPage(WaitPage):
     after_all_players_arrive = 'set_payoffs'
 
-
 class Results(Page):
+
+    def before_next_page(player: Player, timeout_happened):
+        import random
+        participant = player.participant
+        
+        if player.round_number == C.NUM_ROUNDS:
+            selected_round = []
+            while len(selected_round) < 2:
+                n = random.randint(1, C.NUM_ROUNDS)
+                if not n in selected_round:
+                    selected_round.append(n)
+            participant.selected_round = selected_round
+            each_realized_payoff = [float(player.in_round(n).payoff) for n in selected_round]
+            realized_payoff = sum(each_realized_payoff)
+            participant.payoff = realized_payoff
+
+class ShuffleWaitPage(WaitPage):
+    body_text = """
+    他のグループのゲームの終了を待っています。
+    しばらくお待ちください。
+    """
+
+    wait_for_all_groups = True
+
     @staticmethod
-    def vars_for_template(player: Player):
-        return dict(offer=C.ENDOWMENT - player.group.kept)
+    def after_all_players_arrive(subsession: Subsession):
+        subsession.group_randomly()
 
-
-page_sequence = [Introduction, Offer, ResultsWaitPage, Results]
+page_sequence = [
+    Role,
+    WaitRoleCheck,
+    Send,
+    ResultsWaitPage,
+    Results,
+    ShuffleWaitPage
+]
