29bceed4d9482dc53070cfd90d26449cc52add58
KatoPachi <63113684+KatoPachi@users.noreply.github.com>
2022-12-14 15:48:29 +0900

    add settings.py

---
+++ settings.py
@@ -0,0 +1,85 @@
+from os import environ
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'ja'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'JPY'
+USE_POINTS = True
+POINTS_CUSTOM_NAME = 'トークン'
+
+PARTICIPANT_FIELDS = [
+    'selected_round',
+    'first_dictator'
+]
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point = 5,
+    participation_fee = 1000
+)
+
+    
+SESSION_CONFIGS = [
+    dict(
+        name = 'patron_dictator_game_fd',
+        display_name = 'Patron-Dictator Game with First-Moving Dictator',
+        num_demo_participants = 6,
+        first_moving_dictator = True,
+        timeout_seconds = None,
+        app_sequence = [
+            'quiz',
+            'move_exogenous',
+            'survey',
+            'payment_info'
+        ]
+    ),
+    dict(
+        name = 'patron_dictator_game_sd',
+        display_name = 'Patron-Dictator Game with Second-Moving Dictator',
+        num_demo_participants = 6,
+        first_moving_dictator = False,
+        timeout_seconds = None,
+        app_sequence = [
+            'quiz',
+            'move_exogenous',
+            'survey',
+            'payment_info'
+        ]
+    ),
+    dict(
+        name = 'dictator',
+        display_name = 'Dictator Game',
+        num_demo_participants = 4,
+        timeout_seconds = 120, # if you want to run experiments without time-out, then specify `None`
+        app_sequence = [
+            'dictator',
+            'survey',
+            'payment_info'
+        ]
+    )
+]
+
+ROOMS = [
+    dict(
+        name='socio',
+        display_name='Research Institute for Socionetwork Strategies (Kansai University)',
+        participant_label_file='_rooms/socio.txt'
+    )
+]
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+DEMO_PAGE_INTRO_HTML = """
+Economic Experiments developed by Hiroki Kato.
+Source code can be found at <a href='https://github.com/KatoPachi'>Github</a>
+"""
+# don't share this with anybody.
+SECRET_KEY = environ.get('OTREE_SECRET_KEY')
