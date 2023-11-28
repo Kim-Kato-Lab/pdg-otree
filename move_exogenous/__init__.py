e5f23723ab5bcdc3e41d7eeccf77f7d7d230d38b
Hiroki Kato <hkato.econ@gmail.com>
2023-11-29 00:05:59 +0900

    add receiver hope page

--- move_exogenous/__init__.py
+++ move_exogenous/__init__.py
@@ -77,12 +77,19 @@ class Group(BaseGroup):
     belief_1 = models.IntegerField(min=0, label="")
     belief_2 = models.IntegerField(min=0, label="")
     belief_r = models.IntegerField(min=0, label="")
+    hope_r = models.IntegerField(
+        min=0,
+        label = """
+        0から200の間の<b>整数（半角数字）</b>で以下のフォームに入力してください。
+        """
+    )
     send_timeout = models.IntegerField()
     allocation_timeout = models.IntegerField()
     promise_timeout = models.IntegerField()
     belief_1_timeout = models.IntegerField()
     belief_2_timeout = models.IntegerField()
     belief_r_timeout = models.IntegerField()
+    hope_r_timeout = models.IntegerField()
     dictator_first = models.BooleanField()
     dictator_promise = models.BooleanField()
     contractible_s = models.BooleanField()
@@ -104,6 +111,9 @@ def belief_r_max(group: Group):
         return C.ENDOWMENT
     else:
         return C.MAXIMUM_MULTIPLY
+
+def hope_r_max(group: Group):
+    return (C.MAXIMUM_MULTIPLY / 100) * C.ENDOWMENT
 
 class Player(BasePlayer):
     pass
@@ -458,6 +468,35 @@ class SecondBelief(Page):
         else:
             g.belief_2_timeout = 0
 
+class ReceiverHope(Page):
+    form_model = 'group'
+    form_fields = ['hope_r']
+
+    @staticmethod
+    def get_timeout_seconds(player: Player):
+        session = player.session
+        return session.config['timeout_seconds']
+
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.role == C.RECEIVER_ROLE
+    
+    @staticmethod
+    def js_vars(player: Player):
+        return dict(
+            current = player.round_number,
+            max = C.NUM_ROUNDS
+        )
+    
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        g = player.group
+        if timeout_happened:
+            g.hope_r_timeout = 1
+            g.hope_r = random.randint(0, (C.MAXIMUM_MULTIPLY / 100) * C.ENDOWMENT)
+        else:
+            g.hope_r_timeout = 0
+
 class WaitSecondMover(WaitPage):
     template_name = 'move_exogenous/ChoiceWait.html'
     body_text = """
@@ -488,6 +527,7 @@ page_sequence = [
     WaitFirstMover,
     SecondMover,
     SecondBelief,
+    ReceiverHope,
     WaitSecondMover,
     ShuffleWaitPage
 ]
